a0c1062adf668b4fb41091f1aeae441d46d854d5
ram-221 <rameshmalli857.com>
2022-10-20 23:14:16 +0530

    Check two String Equal Or Not

---
+++ src/com/bridgelabz/StringEqualOrNot.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class StringEqualOrNot {
+
+	public static void main(String[] args) {
+		String s1= "String Comparison";
+		String s2 = "String ComParison";
+		System.out.println(s1.equals(s2));
+	}
+}
